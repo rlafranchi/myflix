3c95c9c75edf4951d8a1d5bbdd22131654e3f0a1
rlafranchi <rlafranchi@serenethemes.com>
2014-09-06 09:52:45 -0600

    added should-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,14 +7,5 @@ describe Category do
     expect(Category.first).to eq(category)
   end
 
-  it "has_many videos" do
-    comedies = Category.create(name: "Comedies")
-    south_park = Video.create(name: "South Park", description: "Cartoon Comedy", categories: [comedies])
-    family_guy = Video.create(name: "Family Guy", description: "Created by Seth Mcfarlane", categories: [comedies])
-    expect(comedies.videos).to eq([south_park,family_guy])
-  end
-
-  # it "belongs to many videos" do
-
-  # end
+  it {should have_many(:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,28 +5,7 @@ describe Video do
     v = Video.create(name: 'name', description: 'description' )
     expect(Video.first).to eq(v)
   end
-
-  it "belongs to category" do
-    cat = Category.create(name: 'Dramas')
-    vid = Video.create(name: 'Monk', description: 'drama', categories: [cat])
-    # 1:M expect(vid.category).to eq(cat)
-    expect(vid.categories).to eq([cat])
-  end
-
-  # M:M association
-  # it "has many categories" do
-
-  # end
-
-  it "requires name" do
-    video = Video.create(description: 'description')
-    video.save
-    expect(Video.count).to eq(0)
-  end
-
-  it "requires description" do
-    video = Video.create(name: 'name')
-    video.save
-    expect(Video.count).to eq(0)
-  end
+  it {should have_many(:categories)}
+  it {should validate_presence_of(:name)}
+  it {should validate_presence_of(:description)}
 end
