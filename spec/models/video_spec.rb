018ff3486fd14872b5cc97118645bd14d65bb4e2
rlafranchi <rlafranchi@serenethemes.com>
2014-09-06 08:50:36 -0600

    rspec prefered format

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Video do
   it "should save" do
     v = Video.create(name: 'name', description: 'description' )
-    v.save
-    Video.first.name.should == 'name'
+    expect(Video.first).to eq(v)
   end
 end
