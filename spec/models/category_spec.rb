902dc7c25979ed615f9412087a93a88d5b0d54d3
Richard LaFranchi <rlafranchi@serenethemes.com>
2014-09-06 20:38:27 -0600

    TDD in progress

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,9 +10,26 @@ describe Category do
   it {should have_many(:videos)}
 
   describe "#recent_videos" do
-    it "returns array of videos ordered by created at"
-    it "returns all videos if there are less than six videos"
-    it "returns six videos if there are six or more"
+    it "returns array of videos ordered by created at" do
+      category = Category.create(name: "comedies")
+      family_guy = Video.create(name: "Family Guy", description: "peter griffin", categories: [category])
+      south_park = Video.create(name: "South Park", description: "cartman", categories: [category], created_at: 1.day.ago)
+      expect(Video.recent_videos).to eq([family_guy, south_park])
+    end
+    it "returns all videos if there are less than six videos" do
+      category = Category.create(name: "comedies")
+      family_guy = Video.create(name: "Family Guy", description: "peter griffin", categories: [category])
+      south_park = Video.create(name: "South Park", description: "cartman", categories: [category], created_at: 1.day.ago)
+      expect(Video.recent_videos.count).to eq(category.videos.count)
+    end
+    it "returns six videos if there are more" do
+      category = Category.create(name: "comedies")
+      family_guy = Video.create(name: "Family Guy", description: "peter griffin", categories: [category])
+      south_park = Video.create(name: "South Park", description: "cartman", categories: [category], created_at: 1.day.ago)
+      family_guy_2 = Video.create(name: "Family Guy", description: "peter griffin", categories: [category], created_at: 2.day.ago)
+      south_park_2 = Video.create(name: "South Park", description: "cartman", categories: [category], created_at: 3.day.ago)
+      expect(Video.recent_videos.count).to eq(category.videos.count)
+    end
     it "returns empyt array if no videos"
   end
 end
