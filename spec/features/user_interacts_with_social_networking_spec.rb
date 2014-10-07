67fefa61436bfa7891a530e5cedb27afee0a18e5
Richard LaFranchi <rlafranchi@Richards-MacBook-Pro.local>
2014-10-07 07:29:22 -0600

    feature spec for social networking

---
+++ spec/features/user_interacts_with_social_networking_spec.rb
@@ -0,0 +1,34 @@
+feature "User interacts with social networking" do
+  scenario "appropriate followers show on people page" do
+    comedies = Fabricate(:category, name: "Comedies")
+    south_park = Fabricate(:video, name: "South Park", small_image_url: '/tmp/south_park.jpg', category: comedies)
+    family_guy = Fabricate(:video, name: "Family Guy", small_image_url: '/tmp/family_guy.jpg', category: comedies)
+    futurama = Fabricate(:video, name: "Futuram", small_image_url: '/tmp/futurama.jpg', category: comedies)
+    bob = Fabricate(:user)
+    review = Fabricate(:review, user: bob, video: futurama, rating: 5, content: "good")
+    user = Fabricate(:user)
+    sign_in(user)
+
+    visit videos_path
+
+    find("a[href='/videos/#{futurama.id}']").click
+    page.should have_content bob.name
+
+    click_link bob.name
+    page.should have_content bob.name
+
+    click_link "Follow"
+    page.should have_content bob.name
+
+    visit user_path(bob)
+    page.should_not have_content "Follow"
+
+    visit people_path
+    relationship = Relationship.find_by(leader_id: bob.id, follower_id: user.id)
+    find("a[href='/relationships/#{relationship.id}']").click
+    page.should_not have_content bob.name
+
+    visit user_path(user)
+    page.should_not have_content "Follow"
+  end
+end
