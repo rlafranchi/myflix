a4994dab0063e72ef8431829e269398393ef1b53
Richard LaFranchi <rlafranchi@Richards-MacBook-Pro.local>
2014-10-07 19:20:10 -0600

    added welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      AppMailer.send_welcome_email(@user).deliver
       redirect_to login_path
     else
       render :new
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,6 @@ Myflix::Application.configure do
   config.assets.debug = true
 
   config.eager_load = false
+
+  config.action_mailer.delivery_method = :letter_opener
 end
