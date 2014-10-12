1a8f1cdff8c4414b6b95dfe7967820cd5cfc4e6f
Richard LaFranchi <rlafranchi@Richards-MacBook-Pro.local>
2014-10-11 19:31:42 -0600

    added tokenable module

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,6 @@
 class Invitation < ActiveRecord::Base
+  include Tokenable
   belongs_to :user
   validates_presence_of :email, :name, :message
   validates_uniqueness_of :email
-
-  before_create :generate_token
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
-  end
 end
