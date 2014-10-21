f1e25587b3813b600ac640016c592fac92eb83b6
Richard LaFranchi <rlafranchi@Richards-MacBook-Pro.local>
2014-10-20 20:34:00 -0600

    storage

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,6 +2,7 @@ require 'fog/aws/storage'
 
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
+    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
